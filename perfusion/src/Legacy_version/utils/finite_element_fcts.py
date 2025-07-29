1a40c39c96d691a611e32f3c4ddd4f8701ba5117
Charlotte DEVILLE <dcharlotte92@gmail.com>
2025-07-29 14:33:04 +0100

    refactor(finite_element): divide the set_up_fe_solver2 into smaller functions

--- perfusion/src/Legacy_version/utils/finite_element_fcts.py
+++ perfusion/src/Legacy_version/utils/finite_element_fcts.py
@@ -80,180 +80,123 @@ def alloc_fct_spaces(mesh, fe_degr, **kwarg):
     return Vp, Vvel, v_1, v_2, v_3, p, p_1, p_2, p_3, K1_space, K2_space
 
 
+def read_boundary_conditions(file_path):
+    data = np.loadtxt(file_path, skiprows=1, delimiter=',')
+    data_dimension_greater_than_1 = data.ndim > 1
+    if data_dimension_greater_than_1:
+        b1 = 1000 * data[:, 1]
+        labels = list(data[:, 0])
+    else:
+        b1 = [1000 * data[1]]
+        labels = [data[0]]
+    return data, b1, labels, data_dimension_greater_than_1
+
+
+def apply_dirichlet_BC(V_space, BC_data, BCs, boundaries, data_dimension_greater_than_1, index, boundary_id):
+    if data_dimension_greater_than_1:
+        BCs.append(DirichletBC(V_space, Constant(BC_data[index, 2]), boundaries, boundary_id))
+    else:
+        BCs.append(DirichletBC(V_space, Constant(BC_data[2]), boundaries, boundary_id))
+    return BCs
+
+
+def apply_neumann_BC(mesh, b1, integrals_N, index, v_1, dS, boundary_id):
+    area = assemble(Constant(1) * dS(boundary_id, domain=mesh))
+    b1[index] = b1[index] / area
+    # b1 includes the average surface-normal velocity for the perfusion regions
+    # computed from the volumetric flow rate [mm^3/s] and the surface area [mm^2]
+
+    if b1[index] > 0:
+        integrals_N.append(b1[index] * v_1 * dS(boundary_id))
+
+    return integrals_N
+
+
+def apply_mixed_BC(mesh, boundaries, integrals_N, BCs, BC_data, V_space, b1, dS, v_1, index,
+                   data_dimension_greater_than_1, boundary_id):
+    # Dirichlet (pressure) boundary condition
+    if BC_data[index, 4] == 0:
+        BCs = apply_dirichlet_BC(V_space, BC_data, BCs, boundaries, data_dimension_greater_than_1, index, boundary_id)
+
+    # Neumann (pressure gradient ~ flux) boundary condition
+    elif BC_data[index, 4] == 1:
+        integrals_N = apply_neumann_BC(mesh, b1, integrals_N, index, v_1, dS, boundary_id)
+
+    else:
+        raise Exception("Boundary condition in the BCs.csv file must be 0 (Dirichlet) or 1 (Neumann)")
+    return integrals_N, BCs
+
+
 def set_up_fe_solver2(mesh, boundaries, V, v_1, v_2, v_3, \
                          p, p_1, p_2, p_3, K1, K2, K3, beta12, beta23, \
-                         pa, pv, read_inlet_boundary, inlet_boundary_file, inlet_BC_type,**kwarg):
-    if 'model_type' in kwarg:
-        model_type = kwarg.get('model_type')
-    else:
-        model_type = 'acv'
-    
+                         pa, pv, read_inlet_boundary, inlet_boundary_file, inlet_BC_type, **kwarg):
+    model_type = kwarg.get('model_type', 'acv')
+
+
     # Source terms are equal to zero
     sigma1 = Constant(0.0)
     sigma2 = Constant(0.0)
     sigma3 = Constant(0.0)
-    
-    if model_type == 'acv':
-        # Boundary treatment
-        BCs = []
-        integrals_N = []
-        # Based on inlet boundary file
-        if read_inlet_boundary == True:
-            BC_data = np.loadtxt(inlet_boundary_file,skiprows=1,delimiter=',')
-            if BC_data.ndim>1:
-                b1 = 1000 * BC_data[:,1]
-                boundary_labels = list(BC_data[:,0])
-                n_labels = len(boundary_labels)
-            else:
-                b1 = [1000 * BC_data[1]]
-                boundary_labels = [BC_data[0]]
-                n_labels = len(boundary_labels)
 
-            # Set constant venous pressure
+    BCs = []
+    integrals_N = []
+    V_space = V.sub(0) if model_type == 'acv' else V
+    # Based on inlet boundary file
+    if read_inlet_boundary:
+        BC_data, b1, boundary_labels, data_dimension_greater_than_1 = read_boundary_conditions(inlet_boundary_file)
+        n_labels = len(boundary_labels)
+
+        if model_type == 'acv':
             for i in range(n_labels):
-                BCs.append( DirichletBC(V.sub(2), Constant(pv), boundaries, int(boundary_labels[i])) )
-                
-            # Set dirichlet boundary conditions
-            if inlet_BC_type == 'DBC':
-                for i in range(n_labels):
-                    if BC_data.ndim>1:
-                        BCs.append( DirichletBC(V.sub(0), Constant(BC_data[i,2]), boundaries, int(boundary_labels[i])) )
-                    else:
-                        BCs.append( DirichletBC(V.sub(0), Constant(BC_data[2]), boundaries, int(boundary_labels[i])) )
-            elif inlet_BC_type == 'NBC':
-                # Neumann boundary conditions
-                dS = ds(subdomain_data=boundaries)
-                for i in range(n_labels):
-                    boundary_id = int(boundary_labels[i])
-                    area = assemble( Constant(1)*dS(boundary_id,domain=mesh) )
-                    b1[i] = b1[i]/area
-                # b1 includes the average surface-normal velocity for the perfusion regions
-                # computed from the volumentric flow rate [mm^3/s] and the surface area [mm^2]
-                for i in range(n_labels):
-                    if b1[i]>0:
-                        integrals_N.append(b1[i]*v_1*dS( int(boundary_labels[i]) ))
-            elif inlet_BC_type == 'mixed':
-                # Neumann boundary conditions
-                dS = ds(subdomain_data=boundaries)
-                for i in range(n_labels):
-                    boundary_id = int(boundary_labels[i])
-                    if BC_data[i,4] == 0: # Dirichlet (pressure) boundary condition
-                        if BC_data.ndim>1:
-                            BCs.append( DirichletBC(V.sub(0), Constant(BC_data[i,2]), boundaries, boundary_id) )
-                        else:
-                            BCs.append( DirichletBC(V.sub(0), Constant(BC_data[2]), boundaries, boundary_id) )
-                    elif BC_data[i,4] == 1: # Neumann (pressure gradient ~ flux) boundary condition
-                        area = assemble( Constant(1)*dS(boundary_id,domain=mesh) )
-                        b1[i] = b1[i]/area
-                    else:
-                        raise Exception("Boundary condition in the BCs.csv file must be 0 (Dirichlet) or 1 (Neumann)")
-    
-                # b1 includes the average surface-normal velocity for the perfusion regions
-                # computed from the volumetric flow rate [mm^3/s] and the surface area [mm^2]
-                for i in range(n_labels):
-                    if BC_data[i,4] == 1: # Neumann (pressure gradient ~ flux) boundary condition
-                        if b1[i]>0:
-                            integrals_N.append(b1[i]*v_1*dS( int(boundary_labels[i]) ))
-            else:
-                raise Exception("inlet_BC_type must be Neumann or Dirichlet ('NBC' or 'DBC')")
-            
+                BCs.append(DirichletBC(V.sub(2), Constant(pv), boundaries, int(boundary_labels[i])))
+
+        dS = ds(subdomain_data=boundaries)
+        if inlet_BC_type == 'DBC': # Dirichlet BC
+            for i in range(n_labels):
+                boundary_id = int(boundary_labels[i])
+                BCs = apply_dirichlet_BC(V_space, BC_data, BCs, boundaries, data_dimension_greater_than_1, i, boundary_id)
+
+        elif inlet_BC_type == 'NBC': # Neumann BC
+            for i in range(n_labels):
+                boundary_id = int(boundary_labels[i])
+                integrals_N = apply_neumann_BC(mesh, b1, integrals_N, i, v_1, dS, boundary_id)
+
+        elif inlet_BC_type == 'mixed': # Mixed BC
+            for i in range(n_labels):
+                boundary_id = int(boundary_labels[i])
+                integrals_N, BCs = apply_mixed_BC(mesh, boundaries, integrals_N, BCs, BC_data, V_space, b1, dS, v_1, i,
+                   data_dimension_greater_than_1, boundary_id)
+
         else:
-            boundary_labels, n_labels = suppl_fcts.region_label_assembler(boundaries)
-            for i in range(n_labels):
-                if boundary_labels[i]>2:
-                    # Brain surface boundary
-                    if inlet_BC_type == 'DBC':
-                        BCs.append( DirichletBC(V.sub(0), Constant(pa), boundaries, boundary_labels[i]) )
-                    BCs.append( DirichletBC(V.sub(2), Constant(pv), boundaries, boundary_labels[i]) )
-    
-        """ END - VOLUME FLOW RATE READING/COMPUTATION """
-        
+            raise Exception("inlet_BC_type must be Neumann, Dirichlet or mixed ('NBC', 'DBC' or 'mixed)")
+
+    else:
+        boundary_labels, n_labels = suppl_fcts.region_label_assembler(boundaries)
+        for i in range(n_labels):
+            if boundary_labels[i] > 2:
+                # Brain surface boundary
+                if inlet_BC_type == 'DBC':
+                    BCs.append(DirichletBC(V.sub(0), Constant(pa), boundaries, boundary_labels[i]))
+                BCs.append(DirichletBC(V.sub(2), Constant(pv), boundaries, boundary_labels[i]))
+
+    if model_type == 'acv':
         # Define variational problem
         LHS = \
-        inner(K1*grad(p_1), grad(v_1))*dx + beta12*(p_1-p_2)*v_1*dx \
-        + inner(K2*grad(p_2), grad(v_2))*dx + beta12*(p_2-p_1)*v_2*dx + beta23*(p_2-p_3)*v_2*dx \
-        + inner(K3*grad(p_3), grad(v_3))*dx + beta23*(p_3-p_2)*v_3*dx
-        
-        RHS = sigma1*v_1*dx + sigma2*v_2*dx + sigma3*v_3*dx + sum(integrals_N)
+        inner(K1 * grad(p_1), grad(v_1)) * dx + beta12 * (p_1 - p_2) * v_1 * dx \
+        + inner(K2 * grad(p_2), grad(v_2)) * dx + beta12 * (p_2 - p_1) * v_2 * dx + beta23 * (p_2-p_3) * v_2 * dx \
+        + inner(K3 * grad(p_3), grad(v_3)) * dx + beta23 * (p_3 - p_2) * v_3 * dx
+        RHS = sigma1 * v_1 * dx + sigma2 * v_2 * dx + sigma3 * v_3 * dx + sum(integrals_N)
+
     elif model_type == 'a':
-        # set constant venous pressure
+        # Set constant venous pressure
         p_venous =  Constant(pv)
-        
-        # Boundary treatment
-        BCs = []
-        integrals_N = []
-        # based on inlet boundary file
-        if read_inlet_boundary == True:
-            BC_data = np.loadtxt(inlet_boundary_file,skiprows=1,delimiter=',')
-            if BC_data.ndim>1:
-                b1 = 1000 * BC_data[:,1]
-                boundary_labels = list(BC_data[:,0])
-                n_labels = len(boundary_labels)
-            else:
-                b1 = [1000 * BC_data[1]]
-                boundary_labels = [BC_data[0]]
-                n_labels = len(boundary_labels)
-            
-            # set dirichlet boundary conditions
-            if inlet_BC_type == 'DBC':
-                for i in range(n_labels):
-                    if BC_data.ndim>1:
-                        BCs.append( DirichletBC(V, Constant(BC_data[i,2]), boundaries, int(boundary_labels[i])) )
-                    else:
-                        BCs.append( DirichletBC(V, Constant(BC_data[2]), boundaries, int(boundary_labels[i])) )
-            elif inlet_BC_type == 'NBC':
-                # Neumann boundary conditions
-                dS = ds(subdomain_data=boundaries)
-                for i in range(n_labels):
-                    boundary_id = int(boundary_labels[i])
-                    area = assemble( Constant(1)*dS(boundary_id,domain=mesh) )
-                    b1[i] = b1[i]/area
-                # b1 includes the average surface-normal velocity for the perfusion regions
-                # computed from the volumentric flow rate [mm^3/s] and the surface area [mm^2]
-                for i in range(n_labels):
-                    if b1[i]>0:
-                        integrals_N.append(b1[i]*v_1*dS( int(boundary_labels[i]) ))
-            elif inlet_BC_type == 'mixed':
-                # Neumann boundary conditions
-                dS = ds(subdomain_data=boundaries)
-                for i in range(n_labels):
-                    boundary_id = int(boundary_labels[i])
-                    if BC_data[i,4] == 0: # Dirichlet (pressure) boundary condition
-                        if BC_data.ndim>1:
-                            BCs.append( DirichletBC(V, Constant(BC_data[i,2]), boundaries, boundary_id) )
-                        else:
-                            BCs.append( DirichletBC(V, Constant(BC_data[2]), boundaries, boundary_id) )
-                    elif BC_data[i,4] == 1: # Neumann (pressure gradient ~ flux) boundary condition
-                        area = assemble( Constant(1)*dS(boundary_id,domain=mesh) )
-                        b1[i] = b1[i]/area
-                    else:
-                        raise Exception("Boundary condition in the BCs.csv file must be 0 (Dirichlet) or 1 (Neumann)")
-    
-                # b1 includes the average surface-normal velocity for the perfusion regions
-                # computed from the volumentric flow rate [mm^3/s] and the surface area [mm^2]
-                for i in range(n_labels):
-                    if BC_data[i,4] == 1: # Neumann (pressure gradient ~ flux) boundary condition
-                        if b1[i]>0:
-                            integrals_N.append(b1[i]*v_1*dS( int(boundary_labels[i]) ))
-            else:
-                raise Exception("inlet_BC_type must be Neumann or Dirichlet ('NBC' or 'DBC')")
-            
-        else:
-            boundary_labels, n_labels = suppl_fcts.region_label_assembler(boundaries)
-            for i in range(n_labels):
-                if boundary_labels[i]>2:
-                    # brain surface boundary
-                    if inlet_BC_type == 'DBC':
-                        BCs.append( DirichletBC(V, Constant(pa), boundaries, boundary_labels[i]) )
-    
-        """ END - VOLUME FLOW RATE READING/COMPUTATION """
-        
+
         # Define variational problem
         beta_total = 1 / (1/beta12+1/beta23)
         LHS = \
-        inner(K1*grad(p), grad(v_1))*dx + beta_total*p*v_1*dx
-        
-        RHS = sigma1*v_1*dx + sum(integrals_N) + beta_total*p_venous*v_1*dx
+        inner(K1 * grad(p), grad(v_1)) * dx + beta_total * p * v_1 * dx
+        RHS = sigma1 * v_1 * dx + sum(integrals_N) + beta_total * p_venous * v_1 * dx
+
     else:
         raise Exception("unknown model type: " + model_type)
 
